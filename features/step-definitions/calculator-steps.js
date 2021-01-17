bed194a487717e83c432f39e27591b08198bdab2
Zoe Villanueva <zvillanueva@stratpoint.com>
2021-01-17 16:52:36 +0800

    Code refactor

--- features/step-definitions/calculator-steps.js
+++ features/step-definitions/calculator-steps.js
@@ -7,32 +7,33 @@ const fs = require("fs");
 
 setDefaultTimeout(60 * 1000);
 let driver = new Builder().forBrowser("chrome").build();
-let imagePath;
-let croppedImagePath;
 
 Given("Open chrome browser and start application", async () => {
-    await driver.get(
-      "https://www.online-calculator.com/full-screen-calculator/"
-    );
+  await driver.get(
+    "https://www.online-calculator.com/full-screen-calculator/"
+  );
 });
 
 When("I enter following values and press the = button", async (dataTable) => {
-  let strValue1=dataTable.raw()[0][1];
-  let strValue2=dataTable.raw()[1][1];
-  let strOperator=dataTable.raw()[2][1];
-  let calcFrame = await driver.findElement({ id: "fullframe" });
-  await inputValues(calcFrame, strValue1, strValue2, strOperator);
+  const strValue1 = dataTable.raw()[0][1];
+  const strValue2 = dataTable.raw()[1][1];
+  const strOperator = dataTable.raw()[2][1];
+  const calcFrame = await driver.findElement({ id: "fullframe" });
+  await calcFrame.sendKeys(strValue1);
+  await calcFrame.sendKeys(strOperator);
+  await calcFrame.sendKeys(strValue2);
+  await calcFrame.sendKeys("=");
 });
 
 Then("I should be able to see", async (dataTable)=>{
-  let expectedValue = dataTable.raw()[0][1]
-  sleep.sleep(1) // Implicit wait to ensure screenshot taken is of calculated value
-  await takeScreenshot();
-  let {data: { text }} = await Tesseract.recognize(croppedImagePath, "eng", {
-    logger: (m) => console.log(m),
-  });
-  let calculatedValue = await text;
-  expect(expectedValue).to.equal(calculatedValue.split('\n')[0]);
+  const expectedValue = dataTable.raw()[0][1]
+  // Ensure screenshot taken is of calculated value as no condition can be relied upon to check DOM
+  sleep.sleep(1) 
+  const croppedImagePath = await takeScreenshot();
+  const {data: { text }} = await Tesseract.recognize(croppedImagePath, "eng");
+  // Formats OCR result for comparison
+  const actualValue = text.split("\n")[0].replace(/\s/g, "");
+  expect(expectedValue).to.equal(actualValue);
 })
 
 AfterAll(() => {
@@ -40,34 +41,36 @@ AfterAll(() => {
 });
 
 const takeScreenshot = async () => {
-  let now = new Date().getTime();
-  imagePath = "./reports/" + now + ".png";
-  croppedImagePath = "./reports/" + now + "_cropped.png";
+  const now = new Date().getTime();
+  const imagePath = "./reports/" + now + ".png";
+  const croppedImagePath = "./reports/" + now + "_cropped.png";
 
-  let calcFrame = await driver.findElement(By.id("fullframe"));
+  const calcFrame = await driver.findElement(By.id("fullframe"));
   await driver.switchTo().frame(calcFrame);
-  let canvas = await driver.findElement(By.id("canvas"));
-  let canvasDataURL = await driver.executeScript("return arguments[0].toDataURL()", canvas)
-  var tempCanvas = canvasDataURL.replace(/^data:image\/png;base64,/, "");
+  const canvas = await driver.findElement(By.id("canvas"));
+  const canvasDataURL = await driver.executeScript(() => arguments[0].toDataURL(), canvas)
+  const tempCanvas = canvasDataURL.replace(/^data:image\/png;base64,/, "");
   fs.writeFileSync(imagePath, tempCanvas, "base64");
 
-  let croppedImage = await driver.executeScript(() => {
-    let croppedCanvas = document.createElement('canvas');
-    croppedCanvas.width = arguments[0].width-110;
+  // Crops image to calculator value result to optimise for OCR
+  const croppedImage = await driver.executeScript(() => {
+    const croppedCanvas = document.createElement('canvas');
+    const originalCanvas = arguments[0];
+    // Crop dimensions
+    const paddingWidth = 110;
+    const paddingTop = 60;
+    const paddingLeft = 60
     croppedCanvas.height = 110;
+    croppedCanvas.width = originalCanvas.width - paddingWidth;
+
     croppedCanvas.getContext("2d").drawImage(
-      arguments[0],
-      60, 60, arguments[0].width-110, 110,
-      0, 0, croppedCanvas.width, 110);
+      originalCanvas,
+      paddingTop, paddingLeft, croppedCanvas.width, croppedCanvas.height,
+      0, 0, croppedCanvas.width, croppedCanvas.height
+    );
     return croppedCanvas.toDataURL().replace(/^data:image\/png;base64,/, "");
   }, canvas);
   fs.writeFileSync(croppedImagePath, croppedImage, "base64");  
-}
-
-const inputValues = async(calcFrame, strValue1, strValue2, strOperator) => {
-  await calcFrame.sendKeys(strValue1);
-  await calcFrame.sendKeys(strOperator);
-  await calcFrame.sendKeys(strValue2);
-  await calcFrame.sendKeys("=");
-  return;
+  
+  return croppedImagePath;
 }
